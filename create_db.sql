ec8730bbdf551d2992c5a0bde212b917d7f3fc74
wxjiha <120654528+wxjiha@users.noreply.github.com>
2024-10-20 22:00:14 +0100

    lab edit

--- create_db.sql
+++ create_db.sql
@@ -4,8 +4,11 @@
 CREATE DATABASE IF NOT EXISTS bettys_books;
 USE bettys_books;
 
-# Create the tables
+# Create the books tables
 CREATE TABLE IF NOT EXISTS books (id INT AUTO_INCREMENT,name VARCHAR(50),price DECIMAL(5, 2) unsigned,PRIMARY KEY(id));
+
+# create the users tables
+CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT, username VARCHAR(50)NOT NULL UNIQUE, first_name VARCHAR(50)NOT NULL, last_name VARCHAR(50)NOT NULL, email VARCHAR(100)NOT NULL UNIQUE, hashedPassword VARCHAR(50)NOT NULL, PRIMARY KEY (id));
 
 # Create the app user
 CREATE USER IF NOT EXISTS 'bettys_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,24 @@
 // Create a new router
 const express = require("express")
+const bcrypt = require('bcrypt')
 const router = express.Router()
+const saltRounds = 10
 
 router.get('/register', function (req, res, next) {
     res.render('register.ejs')                                                               
 })    
 
 router.post('/registered', function (req, res, next) {
+    const plainPassword = req.body.password;
+    const username = req.body.username; 
+
+    bcrypt.hash(plainPassword, saltRounds, function (err, hashedPassword) {
+        if (err) {
+            return next(err); 
+        }
+
     // saving data in database
-    res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email)                                                                           
+    res.send(' Hello '+ req.body.first + ' '+ req.body.last + ', your username is ' + username + ' you are now registered!  We will send an email to you at ' + req.body.email + ' Your password has been securely stored.')                                                                           
 })
 
 // Export the router object so index.js can access it
